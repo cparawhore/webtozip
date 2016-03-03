0587f7f452a357655aa982cdd308d9deb311195e
cparawhore <jl.cnblue@gmail.com>
2016-03-03 12:20:54 -0500

    Conexion db de azure
    Credenciales y datos

--- conexion.php
+++ conexion.php
@@ -1,12 +1,12 @@
 <?php
-$mysql_host = "localhost";
-$mysql_database = "turlwebtozip";
-$mysql_user = "root";
-$mysql_password = "";
+$mysql_host = "us-cdbr-azure-central-a.cloudapp.net";
+$mysql_database = "dbarranca";
+$mysql_user = "b6a0edcc69e543";
+$mysql_password = "1fac6bca ";
 #Inicio coneccion a base de datos
    if (!($link=mysql_connect($mysql_host, $mysql_user, $mysql_password))) 
    { 
-      echo "Error conectando a la base de datos."; 
+      echo "Error conectando a la base de datos. gg :V"; 
       exit(); 
    } 
    if (!mysql_select_db($mysql_database,$link)) 
